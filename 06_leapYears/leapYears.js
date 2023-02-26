682fe3a8b1f8b65c8d368ec81df2e008d2650b85
jdmlopes <jdlopes97@gmail.com>
2023-02-26 17:29:21 -0300

    Solve exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //-- A YEAR IS A LEAP YEAR IF --
+    //divisible by 4
+    //not divisible by 100
+    //  unless divisible by 400
+    if(year % 4 === 0 && year % 100 !== 0){
+        return true;
+    }else if(year % 100 === 0 && year % 400 === 0){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
