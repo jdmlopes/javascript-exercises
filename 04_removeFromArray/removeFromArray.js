d957e818a0f30f0b9ba14529b6ae1dae8b1943c6
jdmlopes <jdlopes97@gmail.com>
2023-02-26 16:54:34 -0300

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array = [], ...elementsToRemove) {
+    resultArray = array;
+    elementsToRemove.forEach((e) =>{
+        resultArray = resultArray.filter((element) => element !== e);
+    });
+    return resultArray;
+    
 };
 
 // Do not edit below this line
